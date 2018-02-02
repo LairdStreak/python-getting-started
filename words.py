0ac07787ec088844284ccfbcf5b587d8b9ec6136
LairdStreak <laird.streak@aranzgeo.com>
2018-02-02 14:06:46 +1300

    Update PS Code to test.

---
+++ words.py
@@ -0,0 +1,20 @@
+#!python3
+
+from urllib.request import urlopen
+
+def fetch_words():
+    '''Fetch doc from url and print to console.
+    '''
+    with urlopen('http://sixty-north.com/c/t.txt') as story:
+        story_words = []
+        for line in story:
+            line_words = line.decode('utf-8').split()
+            for word in line_words:
+                story_words.append(word)
+
+    for word in story_words:
+        print(word)
+
+
+if __name__ == '__main__':
+    fetch_words()
